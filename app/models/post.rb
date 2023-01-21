a0241f64d942006b532ca5a02b3dc5247216b8a0
Newton <altontonnalumasa@gmail.com>
2023-01-21 21:33:15 +0300

    add validations to my models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   belongs_to :author, class_name: 'User'
 
+  validates :Title, presence: true
+
   def update_author_post_counter
     author.increment!(:PostsCounter)
   end
