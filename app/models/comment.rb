9b0490d5f78a6a9eb6c99383afe9e896d14bf6e3
Newton <altontonnalumasa@gmail.com>
2023-01-21 20:55:14 +0300

    update my comment to private

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  private
+
+  def update_post_comment_counter
+    post.increment!(:CommentsCounter)
+  end
+end
