d172079ace2c5295cfd1c9da5cf6619ec81267aa
Newton <altontonnalumasa@gmail.com>
2023-02-08 20:18:13 +0300

    Install and set up the devise

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'sprockets-rails'
 # Use sqlite3 as the database for Active Record
 gem 'pg', '~> 1.4.5'
 
+gem 'devise'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_145839) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_165957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_145839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["address"], name: "index_users_on_address"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
