dd84b4d58cbadd8c022465a423481a7b8fe5e265
Newton <altontonnalumasa@gmail.com>
2023-02-09 12:35:08 +0300

    Add confirmable to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[Name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[Name email password Photo Bio])
   end
 end
