ca5ea6c797e8c373a940e1a515c3ef1a9a142857
Newton <altontonnalumasa@gmail.com>
2023-03-28 11:57:22 +0300

    Rearranged users row

--- db/migrate/20230120144741_create_user.rb
+++ db/migrate/20230120144741_create_user.rb
@@ -4,9 +4,9 @@ class CreateUser < ActiveRecord::Migration[7.0]
       t.string :Name
       t.string :Photo
       t.text :Bio
+      t.integer :PostsCounter
       t.datetime :UpdatedAt
       t.datetime :CreatedAt
-      t.integer :PostsCounter
 
       t.timestamps
     end
