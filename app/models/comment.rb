59b1aaf9924a92998dacbf239853eaf98a0a6236
Newton <altontonnalumasa@gmail.com>
2023-03-29 20:13:32 +0300

    Update the models with auto update method on save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
+  after_save :update_post_comments_counter
+
+  private
   def update_post_comment_counter
     post.increment!(:comments_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   belongs_to :author, class_name: 'User'
 
+  after_save :update_author_post_counter
+
   def update_author_post_counter
     author.increment!(:posts_counter)
   end
