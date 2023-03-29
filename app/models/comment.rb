5c81aa52fa6d67c7aabeb67ef2615c3ee8b872a0
Newton <altontonnalumasa@gmail.com>
2023-03-29 18:38:45 +0300

    Add text indent to 3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-  def update_post_comment_counter
-    post.increment!(:comments_counter)
-  end
+   def update_post_comment_counter
+      post.increment!(:comments_counter)
+   end
 end
