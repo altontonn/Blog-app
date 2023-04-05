732dc118c1805370a39374c0d8f374aebc493720
Newton <altontonnalumasa@gmail.com>
2023-04-05 14:10:50 +0300

    write test for controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,19 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'Routes', type: :request do
   describe 'GET /index' do
-    it 'returns a success response' do
-      get users_url
-      expect(response).to have_http_status(:ok)
+    it 'return http success' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get users_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'return body posts' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get users_path(user)
       expect(response.body).to include('Users')
+    end
+
+    it 'return index template' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get users_path(user)
       expect(response).to render_template(:index)
     end
   end
 
-  describe 'users#show' do
-    it 'returns a users record!' do
-      get '/users/1'
-      expect(response).to have_http_status(:ok)
+  describe 'GET /show' do
+    it 'return http success' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'return body posts' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get user_path(user)
       expect(response.body).to include('Username')
+    end
+
+    it 'return index template' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico',
+                         posts_counter: 4)
+      get user_path(user)
       expect(response).to render_template(:show)
     end
   end
