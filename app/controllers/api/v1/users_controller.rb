481328529ee47c3ffd32e1287e0f1eae9822a2ed
Newton <altontonnalumasa@gmail.com>
2023-09-26 01:04:13 +0300

    Configure the devise functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  protect_from_forgery with: :null_session
   def index
     @users = User.all
     render json: @users
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
    # our application routes per the DSL in https://guides.rubyonrails.org/routing.html
    # Defines the root path route ("/")
    root "users#index"
-   # devise_for :users
+   devise_for :users
 
    resources :users, only: [:index, :show] do
       resources :posts, only:[:index, :show, :new, :create, :destroy] do
